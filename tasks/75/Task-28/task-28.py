c5aaf50e61f277c04c576eff114d86a3031190e6
CABlank <carlosblank333@gmail.com>
2024-03-18 05:32:46 -0400

    Two tasks

--- tasks/75/Task-28/task-28.py
+++ tasks/75/Task-28/task-28.py
@@ -1,25 +1,28 @@
-radiant = deque()
-    dire = deque()
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        radiant = deque()
+        dire = deque()
 
-    # Initialize the queues with indices of senators
-    for i, s in enumerate(senate):
-        if s == 'R':
-            radiant.append(i)
-        else:
-            dire.append(i)
+        # Initialize the queues with indices of senators
+        for i, s in enumerate(senate):
+            if s == 'R':
+                radiant.append(i)
+            else:
+                dire.append(i)
 
-    while radiant and dire:
-        # Get the index of the next senator from each party
-        r = radiant.popleft()
-        d = dire.popleft()
+        while radiant and dire:
+            # Get the index of the next senator from each party
+            r = radiant.popleft()
+            d = dire.popleft()
 
-        # The senator with the lower index (i.e., gets to act first) bans the other
-        if r < d:
-            # If the radiant senator acts, push them to the end of the queue with an incremented index
-            radiant.append(r + len(senate))
-        else:
-            # If the dire senator acts, push them to the end of the queue with an incremented index
-            dire.append(d + len(senate))
+            # The senator with the lower index (i.e., gets to act first) bans the other
+            if r < d:
+                # If the radiant senator acts, push them to the end of the queue with an incremented index
+                radiant.append(r + len(senate))
+            else:
+                # If the dire senator acts, push them to the end of the queue with an incremented index
+                dire.append(d + len(senate))
 
-    # Whichever party still has senators left wins
-    return "Radiant" if radiant else "Dire"
+        # Whichever party still has senators left wins
+        return "Radiant" if radiant else "Dire"
